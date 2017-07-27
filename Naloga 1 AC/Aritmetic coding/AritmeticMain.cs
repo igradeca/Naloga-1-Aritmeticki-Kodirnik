53473623e937e48fb31cb0d289a8c35ff5cefee4
Ivan Gradecak <ivan.gradecak@student.um.si>
2017-07-27 23:45:02 +0200

    Encoding table done (probably) but still without creating result. Optimization: none.

--- Naloga 1 AC/Aritmetic coding/AritmeticMain.cs
+++ Naloga 1 AC/Aritmetic coding/AritmeticMain.cs
@@ -10,9 +10,9 @@ namespace Aritmetic_coding
     {
         int encodingBits;
         EncodingInitializationData encodingInit;
-        //List<ProbabilityData> probabilityTable;
         ProbabilityData[] probabilityTable;
         ulong frequenciesSum;
+        byte[] result;
 
         EncodingSimbolData[] encodingTable;
 
@@ -20,7 +20,6 @@ namespace Aritmetic_coding
 
             this.encodingBits = encodingBits;
             encodingInit = new EncodingInitializationData(encodingBits);
-            //probabilityTable = new List<ProbabilityData>();
             probabilityTable = new ProbabilityData[256];
             frequenciesSum = 0;
         }
@@ -83,16 +82,6 @@ namespace Aritmetic_coding
                     previousIndex = i;
                 }
             }
-            /*
-            probabilityTable[0].probability = (float)probabilityTable[0].frequency / (float)frequenciesSum;
-            probabilityTable[0].lowerBoundary = 0;
-            probabilityTable[0].upperBoundary = probabilityTable[0].frequency;
-            for (int i = 1; i < probabilityTable.Length; i++) {
-                probabilityTable[i].probability = (float)probabilityTable[i].frequency / (float)frequenciesSum;
-                probabilityTable[i].lowerBoundary = probabilityTable[i - 1].upperBoundary;
-                probabilityTable[i].upperBoundary = probabilityTable[i].lowerBoundary + probabilityTable[i].frequency;
-            }
-            */
         }
 
         private void SetEncodingTable(byte[] data) {
@@ -101,7 +90,8 @@ namespace Aritmetic_coding
             ulong oldUpperBoundary = encodingInit.maxBound;            
             ulong step;
             ulong newLowerBoundary, newUpperBoundary;
-            ulong E1orE2_Lower = 0, E1orE2_Upper = 0, E3_Lower = 0, E3_Upper = 0;
+            ulong E1E2Lower, E1E2Upper;
+            ulong E3Lower, E3Upper;
             int E3_Counter = 0;
 
             byte simbol;
@@ -109,57 +99,77 @@ namespace Aritmetic_coding
 
                 simbol = data[i];
 
+                E1E2Lower = 0;
+                E1E2Upper = 0;
+                E3Lower = 0;
+                E3Upper = 0;
+
                 step = (oldUpperBoundary - oldLowerBoundary + 1) / frequenciesSum;
                 newUpperBoundary = oldLowerBoundary + (step * probabilityTable[simbol].upperBoundary) - 1;
                 newLowerBoundary = oldLowerBoundary + (step * probabilityTable[simbol].lowerBoundary);
 
-                ScaleIntervals(ref E1orE2_Lower, ref E3, ref E3_Counter, ref newLowerBoundary, ref newUpperBoundary);
+                ScaleIntervals(newLowerBoundary, newUpperBoundary, ref E1E2Lower, ref E1E2Upper, ref E3Lower, ref E3Upper, ref E3_Counter);
 
                 encodingTable[i] = new EncodingSimbolData();
-                //encodingTable[i].Add(simbol, step, oldLowerBoundary, oldUpperBoundary, newLowerBoundary, newUpperBoundary, E1orE2, E3, E3_Counter);
+                encodingTable[i].Add(simbol, step, oldLowerBoundary, oldUpperBoundary, newLowerBoundary, newUpperBoundary, E1E2Lower, E1E2Upper, E3Lower, E3Upper, E3_Counter);
 
-                oldLowerBoundary = newLowerBoundary;
-                oldUpperBoundary = newUpperBoundary;
-            }
-            
+                oldLowerBoundary = E3Lower;
+                oldUpperBoundary = E3Upper;                
+            }            
         }
 
-        private void ScaleIntervals(ref ulong E1orE2, ref ulong E3, ref int E3_Counter, ref ulong lowerBoundary, ref ulong upperBoundary) {
-
-            //bool E1 = false, E2 = false;
-            bool E1AndE2Done = false;
-            while (!E1AndE2Done) {                
-                if (upperBoundary < encodingInit.secondQuater) {        // E1
-                    lowerBoundary *= 2;
+        private void ScaleIntervals(ulong lowerBoundary, ulong upperBoundary, ref ulong E1E2Lower, ref ulong E1E2Upper, ref ulong E3Lower, ref ulong E3Upper, ref int E3_Counter) {
+                        
+            bool E1AndE2Done;
+            do {
+                E1AndE2Done = false;
+                if (upperBoundary < encodingInit.secondQuater) {
+                    lowerBoundary = lowerBoundary * 2;
                     upperBoundary = (upperBoundary * 2) + 1;
                     // na izhod se salje bit 0 in E3_Counter krat bit 1
-                } else if (lowerBoundary >= encodingInit.secondQuater) {        // E2
+                    if (E3_Counter > 0) {
+
+                        E3_Counter = 0;
+                    }
+
+                    E1AndE2Done = true;
+                } else if (lowerBoundary >= encodingInit.secondQuater) {
                     lowerBoundary = 2 * (lowerBoundary - encodingInit.secondQuater);
                     upperBoundary = (2 * (upperBoundary - encodingInit.secondQuater)) + 1;
                     // na izhod se salje bit 1 in E3_Counter krat bit 0
-                } else {
+                    if (E3_Counter > 0) {
+
+                        E3_Counter = 0;
+                    }
+
                     E1AndE2Done = true;
                 }
-            }
+            } while (E1AndE2Done);
+            
+            E1E2Lower = lowerBoundary;
+            E1E2Upper = upperBoundary;
 
             // E3
-            if (lowerBoundary >= encodingInit.firstQuater && upperBoundary < encodingInit.thirdQuarter) {
+            while ((lowerBoundary >= encodingInit.firstQuater) && (upperBoundary < encodingInit.thirdQuarter)) {
                 lowerBoundary = 2 * (lowerBoundary - encodingInit.firstQuater);
                 upperBoundary = 2 * (upperBoundary - encodingInit.firstQuater) + 1;
                 ++E3_Counter;
             }
+
+            E3Lower = lowerBoundary;
+            E3Upper = upperBoundary;       
         }
 
         private void PrintEncodingTable() {
 
-            Console.WriteLine(string.Format("\n  {0,6} {1,7} {2,8} {3,8} {4,8} {5,8} {6,8} {7,5} {8,13}", 
+            Console.WriteLine(string.Format("\n  {0,6} {1,7} {2,8} {3,8} {4,8} {5,8} {6,10} {7,7} {8,13}", 
                 "Simbol", "Step", "Old L", "Old H", "New L", "New H", "E1/E2", "E3", "E3 Counter"));
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------------------------------------------");
 
             for (int i = 0; i < encodingTable.Length; i++) {
-                Console.WriteLine(string.Format("  {0,6} {1,7} {2,8} {3,8} {4,8} {5,8} {6,8} {7,5} {8,13}",
+                Console.WriteLine(string.Format("  {0,6} {1,7} {2,8} {3,8} {4,8} {5,8} {6,10} {7,7} {8,13}",
                     (char)encodingTable[i].simbol, encodingTable[i].step, encodingTable[i].oldLowerBoundary, encodingTable[i].oldUpperBoundary, 
-                    encodingTable[i].newLowerBoundary, encodingTable[i].newUpperBoundary, encodingTable[i].E1orE2, encodingTable[i].E3, encodingTable[i].E3_Counter));
+                    encodingTable[i].newLowerBoundary, encodingTable[i].newUpperBoundary, (encodingTable[i].E1E2Lower + " " + encodingTable[i].E1E2Upper), (encodingTable[i].E3Lower + " " + encodingTable[i].E3Upper), encodingTable[i].E3_Counter));
             }
         }
         /*
--- Naloga 1 AC/Aritmetic coding/EncodingSimbolData.cs
+++ Naloga 1 AC/Aritmetic coding/EncodingSimbolData.cs
@@ -13,11 +13,14 @@ namespace Aritmetic_coding {
         public ulong oldUpperBoundary { get; set; }
         public ulong newLowerBoundary { get; set; }
         public ulong newUpperBoundary { get; set; }
-        public ulong E1orE2 { get; set; }
-        public ulong E3 { get; set; }
+        public ulong E1E2Lower { get; set; }
+        public ulong E1E2Upper { get; set; }
+        public ulong E3Lower { get; set; }
+        public ulong E3Upper { get; set; }
         public int E3_Counter { get; set; }
 
-        public void Add(byte simbol, ulong step, ulong oldLowerBoundary, ulong oldUpperBoundary, ulong newLowerBoundary, ulong newUpperBoundary, ulong E1orE2, ulong E3, int E3_Counter) {
+        public void Add(byte simbol, ulong step, ulong oldLowerBoundary, ulong oldUpperBoundary, ulong newLowerBoundary, ulong newUpperBoundary, 
+            ulong E1E2Lower, ulong E1E2Upper, ulong E3Lower, ulong E3Upper, int E3_Counter) {
 
             this.simbol = simbol;
             this.step = step;
@@ -25,8 +28,10 @@ namespace Aritmetic_coding {
             this.oldUpperBoundary = oldUpperBoundary;
             this.newLowerBoundary = newLowerBoundary;
             this.newUpperBoundary = newUpperBoundary;
-            this.E1orE2 = E1orE2;
-            this.E3 = E3;
+            this.E1E2Lower = E1E2Lower;
+            this.E1E2Upper = E1E2Upper;
+            this.E3Lower = E3Lower;
+            this.E3Upper = E3Upper;
             this.E3_Counter = E3_Counter;
         }
 
